2279112da44ed366cc5f36061292860d8d930933
摆码王子 <1146556298@qq.com>
2019-05-02 01:42:12 +0800

    materialize

---
+++ src/main/java/me/zbl/reactive/rxjava/Materialize.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 ZHENG BAO LE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.zbl.reactive.rxjava;
+
+import io.reactivex.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ZHENG BAO LE
+ * @since 2019-05-02
+ */
+public class Materialize {
+
+    public static void main(String[] args) throws InterruptedException {
+        Observable.intervalRange(0, 4, 0, 1, TimeUnit.SECONDS)
+                .materialize()
+                .doOnNext(n -> System.out.println("value: " + n.getValue() +
+                        ", onNext? " + n.isOnNext() + ", onComplete?" + n.isOnComplete()))
+                .subscribe();
+        Thread.sleep(5_000);
+    }
+}
