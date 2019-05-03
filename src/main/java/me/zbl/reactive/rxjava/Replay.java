c6b9dc79e44d3a0a660b4ed5ab7b71925df8a5b8
摆码王子 <1146556298@qq.com>
2019-05-03 21:28:48 +0800

    replay

---
+++ src/main/java/me/zbl/reactive/rxjava/Replay.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 ZHENG BAO LE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package me.zbl.reactive.rxjava;
+
+import io.reactivex.Observable;
+import io.reactivex.observables.ConnectableObservable;
+
+/**
+ * @author ZHENG BAO LE
+ * @since 2019-05-03
+ */
+public class Replay {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConnectableObservable<Integer> observable = Observable.range(0, 3)
+                .doOnNext(System.out::println)
+                .replay();
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1_000);
+            observable.connect();
+        }
+    }
+}
